84dd2faf40c24c89ef868a150f797b0798749c6d
LAXCartMan <cjweems311@gmail.com>
2021-12-26 08:18:47 -0500

    Bot files

---
+++ src/bot.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ui import Button, View
+from discord.ext import commands
+from dotenv import load_dotenv
+from os import environ
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=".", description="description", intents=intents)
+
+@bot.event
+async def on_ready():
+    print('{0.user} is online'.format(bot))
+
+@bot.event
+async def on_member_join(member):
+    if member.guild.id == 923357625138155541:
+        em = discord.Embed(title= "Welcome " + member.display_name + "!", description=f"Welcome to the Relaxed side of Discord, **" + member.display_name + "**! \n Dont forget to check out:\n" + member.guild.get_channel(923357625633099808).mention + "\n" + member.guild.get_channel(923362097746817025).mention + "\n" + member.guild.get_channel(923362875219796091).mention +"\n", color=discord.Colour.purple())
+
+        em.set_thumbnail(url='https://raw.githubusercontent.com/Downtown-Bot-Developement/Downtown-Bot/main/img/server-icon.gif?token=ARDS46KJCRYRC46RD4FQAE3BYXEFC')
+        em.set_footer(text="Tip: Use /welcome or right click the new member's name, go to apps, and click on welcome to welcome them to the server!")
+
+        channel = bot.get_channel(923362875219796091)
+
+        await channel.send(f"{member.mention}")
+        await channel.send(embed=em)
+
+@bot.slash_command(guild_ids=[923357625138155541])
+async def welcome(ctx, member: discord.Member):
+    await ctx.send(f'{ctx.author.mention} welcomes {member.mention} to {ctx.guild.name}!')
+
+@bot.slash_command(guild_ids=[923357625138155541])
+async def hi(ctx):
+    button = Button(label="click me!", style=discord.ButtonStyle.green, emoji="⚒️")
+    view = View()
+    view.add_item(button)
+    await ctx.send("hi", view=view)
+
+load_dotenv(".env")
+bot.run(environ.get("TOKEN"))
