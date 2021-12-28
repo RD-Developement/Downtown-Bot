ad8ee9da7a845c4d397404d25d3e1988f1253424
LAXCartMan <cjweems311@gmail.com>
2021-12-28 09:55:44 -0500

    Used on Relaxed Downtown

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from os import environ
 import os
+from discord.commands import permissions
 
 intents = discord.Intents.default()
 intents.members = True
@@ -20,6 +21,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
+    # downtown development test server
     if member.guild.id == 923357625138155541:
         em = discord.Embed(title= "Welcome " + member.display_name + "!", description=f"Welcome to the Relaxed side of Discord, **" + member.display_name + "**! \n Dont forget to check out:\n" + member.guild.get_channel(923357625633099808).mention + "\n" + member.guild.get_channel(923362097746817025).mention + "\n" + member.guild.get_channel(923362875219796091).mention +"\n", color=discord.Colour.purple())
 
@@ -30,18 +32,31 @@ async def on_member_join(member):
 
         await channel.send(f"{member.mention}")
         await channel.send(embed=em)
+    # relaxed downtown
+    if member.guild.id == 771797312581402674:
+        em = discord.Embed(title= "Welcome " + member.display_name + "!", description=f"Welcome to the Relaxed side of Discord, **" + member.display_name + "**! \n Dont forget to check out:\n" + member.guild.get_channel(806194833852858378).mention + "\n" + member.guild.get_channel(818131977122480148).mention + "\n" + member.guild.get_channel(818964691945652246).mention +"\n", color=discord.Colour.purple())
+        em.set_thumbnail(url='http://bot.relaxed-downtown.com/img/server-icon.gif')
+        em.set_footer(text="Tip: Use /welcome to welcome them to the server!")
 
-@bot.slash_command(guild_ids=[923357625138155541])
+        channel = bot.get_channel(824363941966250046)
+
+        await channel.send(f"{member.mention}")
+        await channel.send(embed=em)
+    
+    else:
+        return
+
+@bot.slash_command(guild_ids=[923357625138155541, 771797312581402674])
 async def welcome(ctx, member: discord.Member):
     '''Welcomes a member to the server!'''
     await ctx.respond(f'{ctx.author.mention} welcomes {member.mention} to {ctx.guild.name}!')
 
-@bot.slash_command(guild_ids=[923357625138155541])
-async def hi(ctx):
-    button = Button(label="click me!", style=discord.ButtonStyle.green, emoji="⚒️")
-    view = View()
-    view.add_item(button)
-    await ctx.respond("hi", view=view)
+# @bot.slash_command(guild_ids=[923357625138155541])
+# async def hi(ctx):
+#     button = Button(label="click me!", style=discord.ButtonStyle.green, emoji="⚒️")
+#     view = View()
+#     view.add_item(button)
+#     await ctx.respond("hi", view=view)
 
 load_dotenv(".env")
 bot.run(environ.get("TOKEN"))
--- src/cogs/downtown.py
+++ src/cogs/downtown.py
@@ -1,14 +1,26 @@
-from discord.commands import slash_command  # Importing the decorator that makes slash commands.
+from discord.commands import slash_command
 from discord.ext import commands
+import discord
 
 class Downtown(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(guild_ids=[923357625138155541])  # Create a slash command for the supplied guilds.
-    async def ud(self, ctx):
-        await ctx.respond("Hi, this is a slash command from a cog!")
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        vip = after.guild.get_role(925010211331248129)
+        if vip in after.roles and vip not in before.roles:
+            completed_all_levels = after.guild.get_role(925010321947643917)
+            await after.edit(nick=f'🌟 {after.display_name}')
+            
+            em = discord.Embed(title="Congrats On VIP!", description="You just reached the VIP status on Relaxed Downtown! There are many features that come with VIP, such as:", color=discord.Colour.purple())
+            em.add_field(name="Perks", value="> ➜ You will be rewarded with the @VIP's role \n ➜ You can change your nickname in 🎀︱request-a-nick \n ➜ You can choose a color role in 🎨︱color-roles \n ➜ You get access to exclusive chats and VC's (🥥︱beach-party / 🚤︱Yacht Club)") 
+
+            # await after.send('```Hey! You! Yeah You! 😎\n\nLets go you just reached the VIP Status on Relaxed Downtown! 💜 \nAnd Yes we have some Premium features on Relaxed Downtown for VIP users: \n \n-Hidden VIP Chats ⭐ (You can find the VIP category under the Minigames category) \n \n-You can now request a nickname! (The text channel for that can be found in the Support category) 🦋\n \n-Question Of The Day :question: (Can be found in the Off-Topic Category) \n \n-Color Roles (can be found in the category User info) ```')
+            await after.send(embed=em)
+            await after.add_roles(completed_all_levels)
+
+
 
 def setup(bot):
     bot.add_cog(Downtown(bot))
-
