2f5687321ba6004b12811cf9ffe8f51bd3958c61
LAXCartMan <cjweems311@gmail.com>
2021-12-31 13:45:56 -0500

    Add welcome message button

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from os import environ
 import os
 from discord.commands import permissions
+import datetime
 
 intents = discord.Intents.default()
 intents.members = True
@@ -19,26 +20,23 @@ for filename in os.listdir('./cogs'):
 async def on_ready():
     print('{0.user} is online'.format(bot))
 
+class WelcomeButton(View):
+    @discord.ui.button(label="Say welcome!", emoji="<:idkwelcomething:926472273626529922>", style=discord.ButtonStyle.blurple)
+    async def button_callback(self, button, interaction):
+        user = interaction.user
+        await interaction.response.send_message(f'{user.mention} welcomes a new member to {user.guild.name}!')
+
 @bot.event
 async def on_member_join(member):
     if member.guild.id == 771797312581402674:
         channel = bot.get_channel(824363941966250046)
         em = discord.Embed(title= "Welcome " + member.display_name + "!", description=f"Welcome to the Relaxed side of Discord, **" + member.display_name + "**! \n Dont forget to check out:\n" + member.guild.get_channel(806194833852858378).mention + "\n" + member.guild.get_channel(818131977122480148).mention + "\n" + member.guild.get_channel(818964691945652246).mention +"\n", color=discord.Colour.purple())
         em.set_thumbnail(url='http://bot.relaxed-downtown.com/img/server-icon.gif')
-        em.set_footer(text="Tip: Use /welcome to welcome them to the server!")
-
-        welcomeButton = Button(label="Welcome Member", style=discord.ButtonStyle.green)
+        em.timestamp=datetime.datetime.utcnow()
+        em.set_footer(text="Downtown Bot", icon_url="https://bot.relaxed-downtown.com/img/bot-icon.png")
         
-        async def welcomeButton_callback(interaction):
-            user = interaction.user
-            await interaction.response.send_message(f'{user.mention} welcomes {member.mention} to {user.guild.name}!')
-
-        welcomeButton.callback = welcomeButton_callback
-
-        view=View()
-        view.add_item(welcomeButton)
-        await channel.send(f"{member.mention}")
-        await channel.send(embed=em, view=view)
+        view=WelcomeButton()
+        await channel.send(content=f"{member.mention}", embed=em, view=view)
     else:
         return
         
